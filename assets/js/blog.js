be6a9d2a3fd13d6ebc60be6e6b90e5860ac99b67
Trey Wayland <waylandtrey7@gmail.com>
2024-06-22 23:17:05 -0700

    Adding java script to save form data to loval storage.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,34 @@
 console.log("blog");
+const authorEl = document.querySelector('#blogPlaceHolder');
+
+
+let storedBlogPosts = localStorage.getItem('blogPosts');
+
+console.log(storedBlogPosts);
+
+const blogForgDisplayEl = document.createElement('div');
+blogForgDisplayEl.classList.add('blogFormDisplay');
+const disoplayTitleEl = document.createElement('div');
+disoplayTitleEl.classList.add('displayTitle');
+// disoplayTitleEl.textContent = storedBlogPosts.author;
+disoplayTitleEl.textContent = "Title";
+blogForgDisplayEl.appendChild(disoplayTitleEl);
+// li4.textContent = "asd";
+authorEl.appendChild(blogForgDisplayEl);
+
+// console.log(JSON.parse(storedBlogPosts));
+
+// console.log(typeof(storedBlogPosts))
+
+// for (thing in JSON.parse(storedBlogPosts)){
+    // parsed = JSON.parse(thing);
+    // const li4 = document.createElement('li');
+    // console.log(thing);
+
+    // li4.textContent = parsed.author;
+    // console.log(JSON.parse(storedBlogPosts).author);
+
+    // authorEl.appendChild(li4);
+
+// }
+
--- assets/js/form.js
+++ assets/js/form.js
@@ -3,12 +3,38 @@ const authorEl = document.querySelector('#formAuthor');
 const titleEl = document.querySelector('#formTitle');
 const contentEl = document.querySelector('#formContent');
 
+
+let blogPosts = [];
+//  localStorage.getItem('blogPosts');
+
+// let storedBlogPosts = localStorage.getItem('blogPosts');
+
+
 function addNewForm(event){
     event.preventDefault();
-    console.log(authorEl.value);
-    console.log(titleEl.value);
-    console.log(contentEl.value);
-    // window.location.replace("blog.html");
+    
+    if ((authorEl.value == "") || (titleEl.value == "") || (contentEl.value == "")){
+        alert("User Error: Please Complete the Form");
+        return;
+    }
+
+    const blogPost = {
+        author : authorEl.value,
+        title : titleEl.value,
+        content : contentEl.value
+    };
+
+    // console.log(blogPost.author);
+    // console.log(blogPosts);
+    blogPosts.push(JSON.stringify(blogPost))
+    
+    localStorage.setItem('blogPosts', blogPosts);
+
+
+    // console.log(authorEl.value);
+    // console.log(titleEl.value);
+    // console.log(contentEl.value);
+    window.location.replace("blog.html");
 }
 
 formEl.addEventListener('submit', addNewForm);
